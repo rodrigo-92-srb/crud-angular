37abc4cfccc46b5b76780c47958a5068a8fb7ee2
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2022-04-19 17:00:54 -0300

    Creating a Service in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
